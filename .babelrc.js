423be33e9c77bfca49dbacda0e43e2cf6c513e2a
Hwa-J <hj1103.code@gmail.com>
2022-05-04 20:14:51 +0900

    Create project

---
+++ .babelrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: ['@babel/preset-env'],
+  plugins: [
+    ['@babel/plugin-transform-runtime']
+  ]
+}
+
+// npm i -D @babel/core @babel/preset-env @babel/plugin-transform-runtime 
